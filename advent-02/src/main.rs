08ccb656f4c5319dde5e12d03fac40bcccbd0ca6
ateRstones <8556589+ateRstones@users.noreply.github.com>
2024-12-07 14:49:39 +0100

    Day 2 done

--- advent-02/src/main.rs
+++ advent-02/src/main.rs
@@ -28,81 +28,6 @@ fn test_report(report: &Vec<i32>) -> bool {
     return is_safe;
 }
 
-fn generate_compensated_report(report: &Vec<i32>) -> Vec<i32> {
-    let mut compensated_report: Vec<i32> = report.clone();
-
-    println!("Previous {report:?}");
-
-    for i in 1..report.len() {
-        let current_diff = report[i] - report[i-1];
-
-        if i >= 3 {
-            let d = report[i];
-            let c = report[i-1];
-            let b = report[i-2];
-            let a = report[i-3];
-            let dmc = d - c;
-            let dmb = d - b;
-            let cmb = c - b;
-            let cma = c - a;
-            let bma = b - a;
-            let abbc = cmb * bma;
-            let bccd = dmc * cmb;
-            let accd = dmc * cma;
-            let abbd = dmb * bma;
-
-            let mut check_order: [i32; 4] = [-3, -2, -1, 0];
-            let mut check_scores: [i32; 4] = [bccd, abbd, accd, abbc];
-
-            // do a dumb sort, can't be asked
-            for _q in 0..check_order.len() {
-                for g in 1..check_order.len() {
-                    if check_scores[g] > check_scores[g-1] {
-                        let temp = check_scores[g];
-                        let temp_i = check_order[g];
-                        check_scores[g] = check_scores[g-1];
-                        check_scores[g-1] = temp;
-                        check_order[g] = check_order[g-1];
-                        check_order[g-1] = temp_i;
-                    }
-                }
-            }
-
-            // Problem detected
-            if abbc < 0 || bccd < 0 {
-                if check_scores[0] > 0 {
-                    let offset = check_order[0];
-                    println!("Removed {offset}");
-                    compensated_report.remove(i-((-offset) as usize));
-                }
-
-                break;
-            }
-        }
-
-        if current_diff == 0 { // fix 0 diff elements, but just once
-            compensated_report.remove(i);
-            break;
-        }
-
-        if current_diff > 3 {
-            if i == 1 {
-                compensated_report.remove(0);
-                break;
-            }
-
-            else if i == report.len() - 1 {
-                compensated_report.remove(i);
-                break;
-            }
-        }
-    }
-    
-    println!("Compensated {compensated_report:?}");
-
-    return compensated_report;
-}
-
 fn main() {
     let mut safe_reports = 0i32;
     let mut safe_compensated_reports = 0i32;
@@ -117,10 +42,16 @@ fn main() {
             safe_reports += 1;
         }
 
-        let compensated_report = generate_compensated_report(&report);
+        for i in 0..report.len() {
+            let mut one_removed: Vec<i32> = Vec::new();
 
-        if test_report(&compensated_report) {
-            safe_compensated_reports += 1;
+            one_removed.extend_from_slice(&report[..i]);
+            one_removed.extend_from_slice(&report[i+1..]);
+
+            if test_report(&one_removed) {
+                safe_compensated_reports += 1;
+                break;
+            }
         }
     }
 
